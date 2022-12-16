02542d5ec6d2ef42b96a3d4fd9176eddd43c822c
DevER2011 <88580138+DevER2011@users.noreply.github.com>
2022-12-16 19:21:41 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+noseX = 0;
+noseY = 0;
 function preload() {
 }
 
@@ -13,9 +15,12 @@ function setup() {
 }
 function draw() {
   image(video, 0, 0, 640, 400);
+  fill(255,0,0);
+  stroke(255,255,0);
+  circle(noseX, NoseY,20)
 }
 function take_snapshot(){
-save('YouGavemeyouraddress');
+save('lookoutTHEWINDOW:).png');
 }
 
 function modelLoaded(){
@@ -24,7 +29,9 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length > 0){
 console.log(results);
-console.log("nose x = " + results[0].pose.nose.x);
-console.log("nose y = " + results[0].pose.nose.y);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+console.log("nose x = " + noseX);
+console.log("nose y = " + noseY);
 }
 }
